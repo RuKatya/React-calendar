dabfab0039b0e575c2a8ccb452d551fb802522cb
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2021-10-31 01:44:48 +0200

    create project

---
+++ src/pages/Event.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const Event: FC = () => {
+  return <div>Da</div>;
+};
+
+export default Event;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const Login: FC = () => {
+  return <div>Da</div>;
+};
+
+export default Login;
